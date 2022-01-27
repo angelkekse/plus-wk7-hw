e9a6acb9d5c0ee7e4549bbdf07680740c7d56c43
angelkekse <96052630+angelkekse@users.noreply.github.com>
2022-01-27 17:09:42 +1000

    Added search, current location, today's max

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 let now = new Date();
 function formatDate() {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   let day = days[now.getDay()];
   let months = [
     "Jan",
@@ -27,8 +19,50 @@ function formatDate() {
   let month = months[now.getMonth()];
   let clock = now.toLocaleTimeString();
   let dateNum = now.getDate();
-  let dateStatement = `${day}, ${dateNum} ${month} - ${clock}`;
+  let dateStatement = `${day} ${dateNum} ${month} @ ${clock}`;
   return dateStatement;
 }
 let timeDisplay = document.querySelector("#date-and-time");
 timeDisplay.innerHTML = formatDate();
+
+//////////////////////////////////
+
+function newCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-search");
+  document.querySelector("#title-city").innerHTML = input.value;
+  let apiUrlEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiKey = "3afd3b68923f6dfb599ab1fd13851db5";
+  let units = "metric";
+  let apiUrl = `${apiUrlEndpoint}q=${input.value}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(updateCity);
+}
+
+function updateCity(response) {
+  console.log(response); // to find other data
+  let output = Math.round(response.data.main.temp_max);
+  let maxTemp = document.querySelector("#changed-city-input-temp");
+  maxTemp.innerHTML = `${output}°C`;
+  let titleCity = document.querySelector("#title-city");
+  titleCity.innerHTML = response.data.name;
+}
+
+let searchBox = document.querySelector("form");
+searchBox.addEventListener("submit", newCity);
+
+function searchLoc(position) {
+  let apiUrlEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiKey = "3afd3b68923f6dfb599ab1fd13851db5";
+  let units = "metric";
+  let apiUrl = `${apiUrlEndpoint}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(updateCity);
+  console.log(apiUrl);
+}
+
+function getPlaceHere(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLoc);
+}
+
+let currentLoc = document.querySelector("#current-location");
+currentLoc.addEventListener("click", getPlaceHere);
